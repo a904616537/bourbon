384d90a63bd82c52b719bc94a1288993d1bd1389
Kain Shi <kainaltion@Kains-MacBook-Pro.local>
2018-02-28 15:40:50 +0800

    _

---
+++ static/js/script.js
@@ -0,0 +1,29 @@
+//中英文切换
+	var type=navigator.appName
+	if (type=="Netscape"){
+	var lang = navigator.language
+	}
+	else{
+	var lang = navigator.userLanguage
+	}
+	//取得浏览器语言的前两个字母
+	var lang = lang.substr(0,2)
+	setLang(lang);	
+	
+	$('.langToggle').on('click',function(){
+		var lang = $('.langToggle .nav-img').attr('alt');
+		setLang(lang);
+	});
+	
+	
+	function setLang(lang){
+		if(lang == 'en'){
+			$('.langToggle').html('<img src="static/imgs/china.png" class="nav-img" alt="zh"/>');
+			$('.changeLang .zh').hide();
+			$('.changeLang .en').show();
+		}else{
+			$('.langToggle').html('<img src="static/imgs/usa.png" class="nav-img" alt="en"/>');
+			$('.changeLang .en').hide();
+			$('.changeLang .zh').show();
+		}
+	}
